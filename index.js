3a3da7cf77cd44be9878422f87b31e01b161ee6d
TiagoSaiago <saiagodesign@proton.me>
2023-09-05 12:40:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let addToButton = document.getElementById('addToDo');
+let toDocontainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToButton.addEventListener('click', function() {
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDocontainer.appendChild(paragraph);
+    inputField.value = "";
+
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    });
+
+    paragraph.addEventListener('dblclick', function() {
+        paragraph.parentNode.removeChild(paragraph);
+    });
+});
